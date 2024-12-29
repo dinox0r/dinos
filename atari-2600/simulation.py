e04e7565eff215fa1b214950c3c2331adfed25b5
Henry Iguaro <henry@canva.com>
2024-12-29 21:12:41 +1100

    wip

--- atari-2600/simulation.py
+++ atari-2600/simulation.py
@@ -1,32 +1,47 @@
-#/usr/env/bin python3
-x = int(input("x-position? (0-160) "))
-# tia_cycles = x + 68
+#!/usr/bin/env python3
+import sys
+if len(sys.argv) > 1:
+  x = int(sys.argv[1])
+else:
+  x = int(input("x-position? (0-160) "))
 tia_cycles = x + 68 - 9 - 9 - 12 # 9 from sta HMOVE, 9 from sta RESP1, 12 from last div cycle
+#tia_cycles = x + 68 - 9 - 12 # 9 from sta HMOVE, 9 12 from last div cycle
 A = tia_cycles
-print(f"lda #{A} ; A will be loaded with {tia_cycles} in the prev scanline")
-print("sta WSYNC ; start of new scanline")
-print(f"sta HMOVE (3 cpu / 9 tia) (TIA target: {tia_cycles} - 9 = {tia_cycles - 9})")
+print(f"\nlda #{A}      ; A will be loaded with {tia_cycles} in the prev scanline")
+print("sta WSYNC    ; start of new scanline - 0 cpu / 0 tia cycles")
+cpu = 3
+print(f"\n\nsta HMOVE    ; ({cpu} cpu / {3 * cpu} tia) TIA target: {tia_cycles} - 9 = {tia_cycles - 9}")
 tia_cycles -= 9
 loop_counter = 0
-print("\ndivision by 15 loop:")
+print("\n; division by 15 loop:")
 while A - 15 > 0:
   A -= 15
-  print(f"sbc #15 (2 cpu / 6 tia) (TIA target: {tia_cycles} - 6 = {tia_cycles - 6}) A = {A}")
+  cpu += 2
+  print(f"sbc #15      ; ({cpu} cpu / {3 * cpu} tia) TIA target: {tia_cycles} - 6 = {tia_cycles - 6}")
   tia_cycles -= 6
-  print(f"bcs     (3 cpu / 9 tia) (TIA target: {tia_cycles} - 9 = {tia_cycles - 9})")
+  cpu += 3
+  print(f"bcs          ; ({cpu} cpu / {3 * cpu} tia) TIA target: {tia_cycles} - 9 = {tia_cycles - 9}")
+  print(f"             ; A = {A}\n")
   tia_cycles -= 9
   loop_counter += 1
 
-print("-----------")
-print(f"num iterations={loop_counter}")
-print("-----------")
-print("last cycle:")
+print(";-----------")
+print(f"; num iterations = {loop_counter}")
+print(";-----------")
+print("; last cycle:")
 A -= 15
-print(f"sbc #15 (2 cpu / 6 tia) (TIA target: {tia_cycles} - 6 = {tia_cycles - 6}) A = {A} carry set!")
+cpu += 2
+print(f"sbc #15      ; ({cpu} cpu / {3 * cpu} tia) TIA target: {tia_cycles} - 6 = {tia_cycles - 6} A = {A} carry set!")
 tia_cycles -= 6
-print(f"bcs     (2 cpu / 6 tia) (TIA target: {tia_cycles} - 6 = {tia_cycles - 6})")
+cpu += 2
+print(f"bcs          ; NOT TAKEN ({cpu} cpu / {3 * cpu} tia) TIA target: {tia_cycles} - 6 = {tia_cycles - 6}\n")
 tia_cycles -= 6
-print(f"sta RESP1 (3 cpu / 9 tia) (TIA target: {tia_cycles} - 9 = {tia_cycles - 9})")
+cpu += 3
+print(f"sta RESP1    ; ({cpu} cpu / {3 * cpu} tia) TIA target: {tia_cycles} - 9 = {tia_cycles - 9}")
+cpu += 3
 tia_cycles -= 9
 
-print(f"\nTIA target = {tia_cycles}")
+tia_x = cpu * 3 - 68
+print(f"cpu/tia: {cpu}/{cpu * 3}")
+print(f"Target TIA: {x + 68}. Target visible TIA: {x}. *Current* TIA {cpu * 3}. *Current* visible TIA: (TIA - 68): {tia_x}")
+print(f"  fine offset: {x - tia_x} (max 8 to left / 7 right)")
