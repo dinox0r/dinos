b078833956328792c8d6eaa10c774f30d37eb30c
Henry Iguaro <henry@canva.com>
2025-06-25 14:29:38 +1000

    Minor tweaks for the game over and jump sounds

--- atari-2600/src/sounds.asm
+++ atari-2600/src/sounds.asm
@@ -8,24 +8,23 @@ GAME_OVER_SOUND:
 
   ; 1st note
   ; Duration in frames / Tone / Frequency / Volume (max 15)
-  byte #3, #$0E, #4, #3
+  byte #3, #$0E, #3, #3
 
   ; 2nd note
   byte #2, #0, #0, #0
 
   ; 3rd note
-  byte #4, #$0E, #4, #3
+  byte #4, #$0E, #3, #3
 
   ; 4th note
-  byte #2, #$0E, #29, #3
+  byte #2, #$0E, #2, #1
 
   ; end of the sound
   .ds 1
 
 JUMP_SOUND:
   ; Duration in frames / Tone / Frequency / Volume (max 15)
-  ; byte #3, #$04, #31, #2
-  byte #3, #$0D, #9, #2
+  byte #3, #$0D, #10, #1
 
   ; end of the sound
   .ds 1
